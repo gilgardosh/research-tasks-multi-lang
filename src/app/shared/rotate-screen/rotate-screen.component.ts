a7ae5ea5d2d4fe769915e3e74e20ce93bcf02b15
Gil Gardosh <gilgardosh@gmail.com>
2021-01-17 08:17:49 +0200

    add rotate screen component

---
+++ src/app/shared/rotate-screen/rotate-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rotate-screen',
+  templateUrl: './rotate-screen.component.html',
+  styleUrls: ['./rotate-screen.component.scss']
+})
+export class RotateScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
